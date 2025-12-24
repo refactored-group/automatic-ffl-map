395bea0ab0203140a8463fc3d8f39d413383d52b
Giancarlo Byrne <gbyrne@razoyo.com>
2025-12-24 12:45:30 -0600

    Fix: Cert on file showing on map

--- src/components/Dealer/components/Locator/DealerCard.tsx
+++ src/components/Dealer/components/Locator/DealerCard.tsx
@@ -52,7 +52,7 @@ export default function DealerCard(props: DealerCardProps): JSX.Element {
             {dealer.labels.some(label => label === "recent_activity") &&
               <span className="tag recent">Recent Activity</span>
             }
-            {dealer.cert_on_file &&
+            {dealer.uuid &&
               <span className="tag cert-on-file" title="Certificate on file">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
